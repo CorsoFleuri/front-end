15d22f0da793e92399f140a99c19ac1aa5dab1a0
polp37 <paultubajudo@gmail.com>
2025-03-24 14:13:32 +0100

    Feat : parametre.js

---
+++ vue_admin/parametre.js
@@ -0,0 +1,19 @@
+function openModal() {
+  document.getElementById('ticketModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('ticketModal').style.display = 'none';
+}
+
+function saveChanges() {
+  const newTitle = document.getElementById('titre-ticket').value;
+  const newSubtitle = document.getElementById('sous-titre-ticket').value;
+  const newFooter = document.getElementById('pied-page-ticket').value;
+
+  document.getElementById('current-titre').getElementsByTagName('span')[0].innerText = newTitle;
+  document.getElementById('current-sous-titre').getElementsByTagName('span')[0].innerText = newSubtitle;
+  document.getElementById('current-pied-page').getElementsByTagName('span')[0].innerText = newFooter;
+
+  closeModal();
+}
